a9ed1acfd47ed7011880700c25fbb3055e2db007
Sander <sandervdb23@gmail.com>
2024-12-23 20:07:25 +0700

    done

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use("/api", productRoutes);
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 	});
 }
 
