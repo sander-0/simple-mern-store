8d5797aeb14f6039df6a4dfdbb9d15ea71b3c1c8
Sander <sandervdb23@gmail.com>
2024-12-23 22:08:09 +0700

    change api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://sander-simple-store-api46891.vercel.app/",
 			},
 		},
 	},
