8605939a28871442415a364ec020bf2c5bb23737
Sander <sandervdb23@gmail.com>
2024-12-23 14:11:15 +0700

    deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+  build: {
+    outDir: "dist", // Explicitly set the output directory to 'dist'
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+      },
+    },
+  },
 });
