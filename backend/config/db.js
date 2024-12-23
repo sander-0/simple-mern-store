acf3df93a9e4c70f030ea3d05af611f35911b9ce
Sander <sandervdb23@gmail.com>
2024-12-23 15:21:24 +0700

    add env db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
 
 export const db = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URI);
+        const connection = await mongoose.connect(MONGO_URI);
         console.log(`MongoDB connected: ${connection.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
