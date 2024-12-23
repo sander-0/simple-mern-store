cb7337c9e1fcf5856b5b257db5549817fc53857b
Sander <sandervdb23@gmail.com>
2024-12-23 14:22:08 +0700

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Explicitly set the output directory to 'dist'
+    outDir: "frontend/dist",
   },
   server: {
     proxy: {
